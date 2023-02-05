59260deba3785beceb9e2854b3b33262189b6577
nkvy23 <nkvy23@gmail.com>
2023-02-05 23:01:20 +0700

    first commit

---
+++ src/firstTestPackage/FirstTest.java
@@ -0,0 +1,28 @@
+package firstTestPackage;
+
+import org.testng.Assert;
+import org.openqa.selenium.*;
+//import org.testng.annotations.*;
+import org.testng.annotations.Test;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+	public class FirstTest {
+		
+	 	public String baseUrl = "https://www.demo.guru99.com/V4/";
+	    String driverPath = "C:\\driverall\\chromedriver.exe";
+	    public WebDriver driver ;
+  @Test
+  public void verifyHomepageTitle() {
+      
+      System.out.println("launching chrome browser"); 
+      System.setProperty("webdriver.chrome.driver", driverPath);
+      driver = new ChromeDriver();
+      driver.get(baseUrl);
+
+      String expectedTitle = "Guru99 Bank Home Page";
+      String actualTitle = driver.getTitle(); 
+      Assert.assertEquals(actualTitle, expectedTitle);
+      driver.close();
+  }
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+///**
+// * 
+// */
+///**
+// * @author minh.nn250695
+// *
+// */
+//module BankProject {
+//}
